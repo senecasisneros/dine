3d33175e345df37c7a135ce0515992ec9e891c69
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-05-25 11:01:40 -0700

    DefinePlugin-added

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,13 @@ module.exports = {
       compress:{
         warnings: false
       }
-    })
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [

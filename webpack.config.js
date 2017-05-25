2f7ac5252f99cc8a7b22c973c43f28cb75a9d5b4
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-05-24 22:24:41 -0700

    updated webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -23,16 +23,58 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        include: path.join(__dirname, 'src'),
-        loader: 'babel'
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+        include: path.resolve('src'),
       },
-      {test: /(\.css)$/, loaders: ['style', 'css']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
-      // Bootstrap 3 jquery requirement
-      { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports?jQuery=jquery' },
-    ]
-  }
-}
+      {
+        test: /\.s[ac]ss$/,
+        loaders: ['style', 'css', 'sass', 'postcss-loader'],
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+        exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file',
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        loader: 'url?prefix=font/&limit=5000',
+      },
+      {
+        test: /\.tff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream',
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml',
+      },
+      {
+        test: /\.(png|gif|jpg|jpeg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url?limit=10000?',
+      },
+      {
+        test: /\.(woff2?|ttf|eot|svg)(\?[\s\S]+)?$/,
+        loader: 'file?emitFile=false',
+      },
+      {
+        test: /\.(jpe?g|png|giff|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
+        ],
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
+        loader: 'imports?jQuery=jquery',
+      },
+    ],
+  },
+};
